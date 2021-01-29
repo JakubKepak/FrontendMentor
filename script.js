b94a0e7ad862b0c2b58c2d73b73e57a60941cd65
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-01-29 21:54:09 +0100

    add animations on FAQ section

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+const highlightQuestion = (element) => {
+  element.classList.toggle("faq_container__question--open");
+};
+
+const rotateArrow = (element) => {
+  element.classList.toggle("faq_container__arrow_image--open");
+};
+
 const toggleHideAnswer = (element) => {
-  if (element.getAttribute("hidden") == null) {
-    element.setAttribute("hidden", true);
+  console.log(element);
+  if (element.classList.contains("faq_container__answer--closed")) {
+    element.classList.remove("faq_container__answer--closed");
   } else {
-    element.removeAttribute("hidden");
+    element.classList.add("faq_container__answer--closed");
   }
 };
 
-document.querySelectorAll(".faq_container__arrow_image").forEach((item) => {
-  item.addEventListener("click", () =>
-    toggleHideAnswer(item.parentElement.parentElement.nextElementSibling)
-  );
+document.querySelectorAll(".faq_container__question").forEach((item) => {
+  item.addEventListener("click", () => {
+    highlightQuestion(item);
+    toggleHideAnswer(item.nextElementSibling);
+    rotateArrow(item.children[1].children[0]);
+  });
 });
